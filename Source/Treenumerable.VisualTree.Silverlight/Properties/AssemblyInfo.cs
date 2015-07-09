c4c8be2cdf49ab51b6987abf8e02eabeb3e88a68
Jason Boyd <jason.boyd.ce@gmail.com>
2015-07-09 13:55:16 -0400

    Created

---
+++ Source/Treenumerable.VisualTree.Silverlight/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Treenumerable.VisualTree.Silverlight")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Toshiba")]
+[assembly: AssemblyProduct("Treenumerable.VisualTree.Silverlight")]
+[assembly: AssemblyCopyright("Copyright © Toshiba 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fb5d0f8c-8a8c-41d1-9eb6-a583734b48d5")]
+
+// Version information for an assembly consists of the following three values:
+//
+//      Major
+//      Minor 
+//      Patch
+//
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
---
+++ Source/Treenumerable.VisualTree.Silverlight/VisualTreeWalker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Treenumerable.VisualTree
+{
+    public class VisualTreeWalker : ITreeWalker<DependencyObject>
+    {
+        public IEnumerable<DependencyObject> GetChildren(DependencyObject node)
+        {
+            int count = VisualTreeHelper.GetChildrenCount(node);
+            for (int i = 0; i < count; i++ )
+            {
+                yield return VisualTreeHelper.GetChild(node, i);
+            }
+        }
+
+        public bool TryGetParent(DependencyObject node, out DependencyObject parent)
+        {
+            parent = VisualTreeHelper.GetParent(node);
+            return parent != null;
+        }
+    }
+}
